14f880d08bf9a1603eb54e31b37dc735a5cebdf3
npet03001 <ni.schum@googlemail.com>
2025-05-12 13:00:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',   // <<< Wichtig für Vercel & GitHub Deployments
+  base: './',
   plugins: [react()],
 });
